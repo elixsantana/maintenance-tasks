896c75e37c7612e5d08cf786cb31811f7b0b2098
Elix Santana <elixsantana@gmail.com>
2023-05-13 16:25:31 -0400

    Creating http server and api handler

--- api/api.go
+++ api/api.go
@@ -1,1 +1,21 @@
 package api
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Handler struct {
+}
+
+func CreateHandler() (*Handler, error) {
+	return &Handler{}, nil
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL)
+	if r.URL.Path == "/" {
+		fmt.Fprintf(w, "Hello World from an API!")
+		fmt.Println("API hit")
+	}
+}
--- main.go
+++ main.go
@@ -2,12 +2,42 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"maintenance-tasks/api"
+	"net/http"
+	"sync"
 )
 
+type handler struct {
+	apiHandler http.Handler
+}
+
 func main() {
-	for {
-		time.Sleep(5 * time.Second)
-		fmt.Println("hello world")
+	apiHandler, err := api.CreateHandler()
+	if err != nil {
+		panic(err)
 	}
+
+	h := handler{
+		apiHandler: apiHandler,
+	}
+
+	srv := &http.Server{Addr: ":3000", Handler: h}
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	fmt.Println("Starting server")
+	go startServer(&wg, srv)
+
+	wg.Wait()
+}
+
+func startServer(wg *sync.WaitGroup, srv *http.Server) {
+	defer wg.Done()
+	err := srv.ListenAndServe()
+	if err != http.ErrServerClosed {
+		panic(err)
+	}
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.apiHandler.ServeHTTP(w, r)
 }
