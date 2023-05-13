5197b452866865faa79d54618f6341807f2a10c2
Elix Santana <elixsantana@gmail.com>
2023-05-13 16:51:54 -0400

    Adding /tasks endpoint

--- api/api.go
+++ api/api.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -17,5 +18,9 @@ func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path == "/" {
 		fmt.Fprintf(w, "Hello World from an API!")
 		fmt.Println("API hit")
+	} else if r.URL.Path == "/tasks" {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(`{"Role":"Manager","Task":"Hello","Time":123456789}`)
 	}
+
 }
